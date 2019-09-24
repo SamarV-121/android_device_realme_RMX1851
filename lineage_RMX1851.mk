16db1b7cc22de919e32c9b24997de9f36a6e0c8e
SamarV-121 <samarvispute121@gmail.com>
2019-09-24 12:27:11 +0000

    RMX1851: Init trees

---
+++ lineage_RMX1851.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2018 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common LOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, device/realme/RMX1851/device.mk)
+
+PRODUCT_NAME := lineage_RMX1851
+PRODUCT_DEVICE := RMX1851
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme 3 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+TARGET_VENDOR_PRODUCT_NAME := RMX1851
+TARGET_VENDOR_DEVICE_NAME := RMX1851
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="RMX1851" \
+    PRODUCT_NAME="RMX1851"
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20190519.045455 release-keys"
+BUILD_FINGERPRINT := Realme/RMX1851/RMX1851:9/PKQ1.190101.001/1557221182:user/release-keys
